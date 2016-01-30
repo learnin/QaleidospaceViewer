9659a9d5206a6154ef75fa3689ad66b6ac20d2e1
learnin <i.learnin@gmail.com>
2016-01-31 00:18:21 +0900

    Initial Commit

---
+++ QaleidospaceViewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QaleidospaceViewer
+//
+//  Created by learn on 2016/01/31.
+//  Copyright © 2016年 Manabu Inoue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
