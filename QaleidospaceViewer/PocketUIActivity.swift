ec7373cd37ef6d8a984723b2195531de8a76774e
learnin <i.learnin@gmail.com>
2016-04-24 00:26:53 +0900

    [modify] Add to show alert dialog when saved to Pocket

--- QaleidospaceViewer/PocketUIActivity.swift
+++ QaleidospaceViewer/PocketUIActivity.swift
@@ -38,10 +38,27 @@ class PocketUIActivity: UIActivity {
                 if (error != nil) {
                     self.activityDidFinish(false)
                 } else {
+                    self.showSuccessAlert()
                     self.activityDidFinish(true)
                 }
             })
         }
+    }
+    
+    func showSuccessAlert() {
+        let alert: UIAlertController = UIAlertController(title: "sent!", message: nil, preferredStyle:  UIAlertControllerStyle.Alert)
+        let defaultAction: UIAlertAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:{
+            (action: UIAlertAction!) -> Void in
+            print("OK")
+        })
+        alert.addAction(defaultAction)
+        
+        // 親のViewControllerを探しだす
+        var baseView: UIViewController = UIApplication.sharedApplication().keyWindow!.rootViewController!
+        while (baseView.presentedViewController != nil && !baseView.presentedViewController!.isBeingDismissed()) {
+            baseView = baseView.presentedViewController!
+        }
+        baseView.presentViewController(alert, animated: true, completion: nil)
     }
 
 }
