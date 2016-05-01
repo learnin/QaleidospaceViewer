a85522a519cd18dc8949ac6f12227827b3a217a1
learnin <i.learnin@gmail.com>
2016-05-01 23:08:10 +0900

    [modify] fix warning

--- QaleidospaceViewer/Qaleidospace.swift
+++ QaleidospaceViewer/Qaleidospace.swift
@@ -15,7 +15,7 @@ public protocol APIEndpoint {
     var path: String { get }
     var method: HTTPMethod { get }
     var parameters: Parameters { get }
-    typealias ResponseType: JSONDecodable
+    associatedtype ResponseType: JSONDecodable
 }
 
 /**
